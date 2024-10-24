c2cc72707c14dd3c9529652b6f77522e1f69736e
hasinduvindana <hasinduvindana@gmail.com>
2024-10-24 22:33:33 +0530

    index page interface update
    nav bar , signin button, signup button,bg image, updated

---
+++ quickjobweb/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function LoginPage() {
+  return (
+    <div>
+      login
+        <Image src={'/image.png'} alt={'img'} width={300} height={300}/>
+    </div>
+  )
+}
